79cc1e5d96cdcae517c1f47bc4ebc6ac9abbc6b4
jonghyeok-97 <dkswhdgur1209@naver.com>
2023-11-10 18:22:31 +0900

    feat: 날짜가 1~31이 아니면 예외발생 기능 구현

---
+++ src/main/java/christmas/Model/VisitDate.java
@@ -0,0 +1,30 @@
+package christmas.Model;
+
+public class VisitDate {
+    private final int visitDate;
+
+    public VisitDate(String inputDate) {
+        validate(inputDate);
+        this.visitDate = Integer.parseInt(inputDate);
+    }
+
+    private void validate(String inputDate) throws IllegalArgumentException {
+        validateNumber(inputDate);
+        validateVisitDateRange(inputDate);
+    }
+
+    private void validateNumber(String inputDate) {
+        try {
+            Integer.parseInt(inputDate);
+        } catch (NumberFormatException error) {
+            throw new IllegalArgumentException("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
+        }
+    }
+
+    private void validateVisitDateRange(String inputDate) {
+        int date = Integer.parseInt(inputDate);
+        if (date < 1 || date > 31) {
+            throw new IllegalArgumentException("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
+        }
+    }
+}
