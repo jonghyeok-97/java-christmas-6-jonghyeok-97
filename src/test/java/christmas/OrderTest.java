5f615ecd784b6b08e9a009f40559485c793e7d3d
jonghyeok-97 <dkswhdgur1209@naver.com>
2023-11-10 19:06:45 +0900

    test: ,가 마지막에 오면 예외 발생하는 테스트 추가

--- src/test/java/christmas/OrderTest.java
+++ src/test/java/christmas/OrderTest.java
@@ -9,10 +9,11 @@ import org.junit.jupiter.params.provider.ValueSource;
 public class OrderTest {
 
     @ParameterizedTest
-    @ValueSource(strings = {",고기-3,닭-1,파스타-3", "고기-3,,파스타2"})
-    @DisplayName("쉼표가 연속되면 오류 발생하는 테스트")
+    @ValueSource(strings = {",고기-3,닭-1,파스타-3", "고기-3,,파스타2", "고기-2,파스타-1,"})
+    @DisplayName("구분자 사이에 값이 없거나 구분자가 끝에오면 오류 발생하는 테스트")
     void create_Order(String input) {
         Assertions.assertThatThrownBy(() -> new Order(input))
                 .isInstanceOf(IllegalArgumentException.class);
     }
+
 }
