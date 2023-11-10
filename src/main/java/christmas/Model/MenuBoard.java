78ef21b6ceb2a2788d785425df220351c43e2fba
jonghyeok-97 <dkswhdgur1209@naver.com>
2023-11-10 23:20:06 +0900

    feat: 메뉴판에 없는 메뉴가 있으면 예외 처리 기능 구현

---
+++ src/main/java/christmas/Model/MenuBoard.java
@@ -0,0 +1,23 @@
+package christmas.Model;
+
+import java.util.Map;
+import java.util.stream.Stream;
+
+public enum MenuBoard {
+    APPETIZER(Map.of("양송이수프", 6000, "타파스", 5500, "시저샐러드", 8000)),
+    MAIN(Map.of("티본스테이크", 55000, "바비큐립", 54000,
+            "해산물파스타", 35000, "크리스마스파스타", 25000)),
+    DESSERT(Map.of("초코케이크", 15000, "아이스크림", 5000)),
+    BEVERAGE(Map.of("제로콜라", 3000, "레드와인", 60000, "샴페인", 25000));
+
+    private final Map<String, Integer> menu;
+
+    MenuBoard(Map<String, Integer> menu) {
+        this.menu = menu;
+    }
+
+    public static boolean compare(String menu) {
+        return Stream.of(MenuBoard.values())
+                .anyMatch(menus -> menus.menu.keySet().contains(menu));
+    }
+}
