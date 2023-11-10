335eb3a07910259387daf159d46136102fbfaa13
jonghyeok-97 <dkswhdgur1209@naver.com>
2023-11-10 20:07:06 +0900

    test: -로 분리했을 때의 테스트 추가

--- src/test/java/christmas/OrderTest.java
+++ src/test/java/christmas/OrderTest.java
@@ -9,11 +9,18 @@ import org.junit.jupiter.params.provider.ValueSource;
 public class OrderTest {
 
     @ParameterizedTest
-    @ValueSource(strings = {",고기-3,닭-1,파스타-3", "고기-3,,파스타2", "고기-2,파스타-1,"})
-    @DisplayName("구분자 사이에 값이 없거나 구분자가 끝에오면 오류 발생하는 테스트")
+    @ValueSource(strings = {",고기-3,닭-1,파스타-3", "고기-3,,파스타2", "고기-2,파스타-1,", "고기-2, ,파스타-3"})
+    @DisplayName("구분자(,) 사이에 값이 없거나 구분자가 끝에오면 오류 발생하는 테스트")
     void create_Order(String input) {
         Assertions.assertThatThrownBy(() -> new Order(input))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
+    @ParameterizedTest
+    @ValueSource(strings = {"고기- 3,닭-1,파스타-3", "-3,파스타-2", "고기2,파스타-1", "고기-,파스타-2"})
+    @DisplayName("구분자(-) 로 분리한 값이 올바르지 않으면 오류 발생하는 테스트")
+    void create_Order2(String input) {
+        Assertions.assertThatThrownBy(() -> new Order(input))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 }
