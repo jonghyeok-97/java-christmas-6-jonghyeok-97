0267cb0c54a0124b421d689d4206238660d1b53c
jonghyeok-97 <dkswhdgur1209@naver.com>
2023-11-11 18:56:38 +0900

    feat: 입력 받는 기능 구현

---
+++ src/main/java/christmas/Controller/EventPlanner.java
@@ -0,0 +1,43 @@
+package christmas.Controller;
+
+import christmas.Model.Order;
+import christmas.Model.VisitDate;
+import christmas.View.InputView;
+import christmas.View.OutputView;
+
+public class EventPlanner {
+    private InputView inputView;
+    private OutputView outputView;
+
+    public EventPlanner() {
+        this.inputView = new InputView();
+        this.outputView = new OutputView();
+    }
+
+    public void start() {
+        System.out.println("안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.");
+        VisitDate visitDate = createVisitDate();
+        Order order = createOrder();
+
+
+
+    }
+
+    private VisitDate createVisitDate() {
+        try {
+            return new VisitDate(inputView.readDate());
+        } catch (IllegalArgumentException error) {
+            System.out.println(error.getMessage());
+            return createVisitDate();
+        }
+    }
+
+    private Order createOrder() {
+        try {
+            return new Order(inputView.readOrder());
+        } catch (IllegalArgumentException error) {
+            System.out.println(error.getMessage());
+            return createOrder();
+        }
+    }
+}
