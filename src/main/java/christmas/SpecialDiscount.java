1b6884fa810f654879ee8bcf4ea21d91870acffc
jonghyeok-97 <dkswhdgur1209@naver.com>
2023-11-12 16:36:48 +0900

    feat: 주문금액에서 -1000원을 할 수 있도록 하는 기능 구현

--- src/main/java/christmas/SpecialDiscount.java
+++ src/main/java/christmas/SpecialDiscount.java
@@ -5,10 +5,13 @@ import christmas.Model.VisitDate;
 import org.mockito.internal.matchers.Or;
 
 public class SpecialDiscount {
+    private boolean isSpecialDiscount;
+    private int specialDiscountPrice;
 
     public SpecialDiscount(VisitDate visitDate, Order order) {
         if (visitDate.isSpecialDate() && order.isOverMinDiscountPrice()) {
-
+            this.isSpecialDiscount = true;
+            this.specialDiscountPrice = 1000;
         }
     }
 }
