bfb48f0814c3b1eb66e03b45bbf34af475fb3ad1
jonghyeok-97 <dkswhdgur1209@naver.com>
2023-11-12 11:44:40 +0900

    refactor: 평일에 포함되는 날짜들 추가

--- src/main/java/christmas/December.java
+++ src/main/java/christmas/December.java
@@ -4,8 +4,9 @@ import java.util.List;
 import java.util.stream.Stream;
 
 public enum December {
-    NORMAL(List.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25)),
-    WEEKDAY(List.of(4, 5, 6, 7, 11, 12, 13, 14, 18, 19, 20, 21, 26, 27, 28)),
+    NORMAL(List.of(
+            1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25)),
+    WEEKDAY(List.of(3, 4, 5, 6, 7, 10, 11, 12, 13, 14, 17, 18, 19, 20, 21, 24, 25, 26, 27, 28, 31)),
     WEEKEND(List.of(1, 2, 8, 9, 15, 16, 22, 23, 29, 30)),
     SPECIAL(List.of(3, 10, 17, 24, 25, 31));
 
@@ -21,7 +22,8 @@ public enum December {
     }
 
     public static boolean checkWeekdaysDate(int visitDate) {
-        return true;
+        return Stream.of(December.WEEKDAY)
+                .anyMatch(week -> week.discountDates.contains(visitDate));
     }
 
 }
