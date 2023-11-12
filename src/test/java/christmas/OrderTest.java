db9ed2c730ca371ce9c83308538ed9c9e87c521e
jonghyeok-97 <dkswhdgur1209@naver.com>
2023-11-12 13:42:30 +0900

    test: 메뉴별로 개수를 저장하는 기능의 테스트 추가

--- src/test/java/christmas/OrderTest.java
+++ src/test/java/christmas/OrderTest.java
@@ -4,9 +4,11 @@ import christmas.Model.MenuBoard;
 import christmas.Model.Order;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import org.assertj.core.api.Assertions;
@@ -67,11 +69,31 @@ public class OrderTest {
     }
 
     @ParameterizedTest
+    @DisplayName("메뉴에 따라 개수가 잘 넣어지는지 테스트")
+    @CsvSource(value = {"양송이수프-3,바비큐립-2:양송이수프,바비큐립:3,2",
+            "초코케이크-5,타파스-1,제로콜라-2:초코케이크,타파스,제로콜라:5,1,2"}, delimiter = ':')
+    void check_countByOrderedMenu(String orderedMenu, String menus, String counts) {
+        Order order = new Order(orderedMenu);
+        Map<String, Integer> countByOrderedMenu = order.getCountByOrderedMenu();
+
+        Set<String> actualMenus = countByOrderedMenu.keySet();
+        Set<String> expectedMenus = Stream.of(menus.split(",")).collect(Collectors.toSet());
+
+        List<Integer> actualCounts = countByOrderedMenu.values().stream().sorted().toList();
+        List<Integer> expectedCounts = Stream.of(counts.split(","))
+                .map(Integer::parseInt)
+                .sorted()
+                .collect(Collectors.toList());
+
+        Assertions.assertThat(actualMenus).isEqualTo(expectedMenus);
+        Assertions.assertThat(actualCounts).isEqualTo(expectedCounts);
+    }
+    @ParameterizedTest
     @DisplayName("메뉴타입에 따라 메뉴가격이 잘 합산되는지 테스트")
     @CsvSource(value = {"양송이수프,타파스,시저샐러드:APPETIZER:19500", "티본스테이크,바비큐립,해산물파스타,제로콜라:MAIN:144000",
             "초코케이크,아이스크림,레드와인,바비큐립:DESSERT:20000"}, delimiter = ':')
     void check_priceByOrderedType(String orderedMenu, MenuBoard MENUTYPE, int totalPriceByType) {
-        Map<MenuBoard, Integer> priceByOrderedType= new HashMap<>();
+        Map<MenuBoard, Integer> priceByOrderedType = new HashMap<>();
         List<String> menus = Stream.of(orderedMenu.split(","))
                 .collect(Collectors.toList());
         for (String menu : menus) {
@@ -83,6 +105,19 @@ public class OrderTest {
 
         int actual = priceByOrderedType.get(MENUTYPE);
         int expected = totalPriceByType;
+
+        Assertions.assertThat(actual).isEqualTo(expected);
+    }
+
+    @ParameterizedTest
+    @DisplayName("디저트 메뉴들의 총 가격 - 메뉴 1개당 * 2023을 계산하는 테스트")
+    @CsvSource(value = {"해산물파스타-1,바비큐립-2,아이스크림-2,레드와인-2:0", "타파스-1,초코케이크-3,제로콜라-5:26793"},
+            delimiter = ':')
+    void calculate_WeekDays_Discount(String menus, int discountPrice) {
+        Order order = new Order(menus);
+
+        int actual = order.calculateWeekdaysDiscount();
+        int expected = discountPrice;
 
         Assertions.assertThat(actual).isEqualTo(expected);
     }
