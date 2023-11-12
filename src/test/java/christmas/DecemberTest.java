940948b1ccf557026c3ac18801e831ef791b5a12
jonghyeok-97 <dkswhdgur1209@naver.com>
2023-11-12 11:41:28 +0900

    test: 크리스마스디데이만 할인되는지 확인하는 테스트 추가

--- src/test/java/christmas/DecemberTest.java
+++ src/test/java/christmas/DecemberTest.java
@@ -1,27 +1,21 @@
 package christmas;
 
 import java.util.List;
-import java.util.stream.Stream;
 import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class DecemberTest {
 
-    @Test
-    void 날짜에_따른_할인유형_테스트_1() {
-        List<December> actual = December.checkDiscountType(7);
-        List<December> expected = List.of(December.D_DAY, December.WEEKDAY);
-
-        Assertions.assertThat(actual).isEqualTo(expected);
-    }
-
-    @Test
-    void 날짜에_따른_할인유형_테스트_2() {
-        List<December> actual = December.checkDiscountType(22);
-        List<December> expected = List.of(December.D_DAY, December.WEEKEND);
+    @ParameterizedTest
+    @DisplayName("크리스마스디데이 날짜만 참인지 확인하는 테스트")
+    @CsvSource(value = {"1,true", "10,true", "25,true", "26,false", "31,false"})
+    void check_not_normalDate(int visitDate, boolean isNormalDate) {
+        boolean actual = December.checkNormalDate(visitDate);
+        boolean expected = isNormalDate;
 
         Assertions.assertThat(actual).isEqualTo(expected);
     }
