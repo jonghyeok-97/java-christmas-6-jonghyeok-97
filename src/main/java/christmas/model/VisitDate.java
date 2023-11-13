fba80725a3d30b5fbad58d4ed125e1d816cbeb6a
jonghyeok-97 <dkswhdgur1209@naver.com>
2023-11-14 00:59:42 +0900

    refactor: 에러메시지를 상수로 변경

--- src/main/java/christmas/model/VisitDate.java
+++ src/main/java/christmas/model/VisitDate.java
@@ -3,7 +3,7 @@ package christmas.model;
 public class VisitDate {
     private static final int FIRST_DATE_OF_DECEMBER = 1;
     private static final int LAST_DATE_OF_DECEMBER = 31;
-    private static final String ERROR_INVALID_DATE = "[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.";
+    private static final String ERROR_RETRY_INPUT = "[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.";
 
     private final int date;
 
@@ -41,14 +41,14 @@ public class VisitDate {
         try {
             Integer.parseInt(inputDate);
         } catch (NumberFormatException error) {
-            throw new IllegalArgumentException(ERROR_INVALID_DATE);
+            throw new IllegalArgumentException(ERROR_RETRY_INPUT);
         }
     }
 
     private void validateDateRange(String inputDate) {
         int date = Integer.parseInt(inputDate);
         if (date < FIRST_DATE_OF_DECEMBER || date > LAST_DATE_OF_DECEMBER) {
-            throw new IllegalArgumentException(ERROR_INVALID_DATE);
+            throw new IllegalArgumentException(ERROR_RETRY_INPUT);
         }
     }
 }
