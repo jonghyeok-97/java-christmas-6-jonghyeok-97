80ddb5c91e23251b33690a2d3bf38d4a652bbf99
jonghyeok-97 <dkswhdgur1209@naver.com>
2023-11-13 17:54:27 +0900

    refactor: 출력 형식에 맞게 주말할인내역 계산수식 변경

--- src/main/java/christmas/model/dateDiscount/WeekendDiscount.java
+++ src/main/java/christmas/model/dateDiscount/WeekendDiscount.java
@@ -5,13 +5,13 @@ import christmas.model.Order;
 import christmas.model.VisitDate;
 
 public class WeekendDiscount extends DateDiscount{
-
     private int weekendDiscountPrice;
+
     public WeekendDiscount(VisitDate visitDate, Order order) {
-        if (visitDate.isWeekendDate() && order.isOverMinDiscountPrice()) {
+        if (visitDate.isWeekendDate()) {
             int mainPrice = order.findPriceOfMain();
             int countMainMenu = order.countMainMenu();
-            this.weekendDiscountPrice = mainPrice - countMainMenu * 2023;
+            this.weekendDiscountPrice = countMainMenu * 2023;
         }
     }
 
