14945f3bfeb27e1f0505f6ceb4cffc6bfb63b84f
jonghyeok-97 <dkswhdgur1209@naver.com>
2023-11-13 23:24:14 +0900

    refactor: 출력메시지 상수화

---
+++ src/main/java/christmas/view/InputView.java
@@ -0,0 +1,19 @@
+package christmas.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+    private static final String INPUT_DATE = "12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)";
+    private static final String INPUT_ORDER =
+            "주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)";
+
+    public String readDate() {
+        System.out.println(INPUT_DATE);
+        return Console.readLine();
+    }
+
+    public String readOrder() {
+        System.out.println(INPUT_ORDER);
+        return Console.readLine();
+    }
+}
