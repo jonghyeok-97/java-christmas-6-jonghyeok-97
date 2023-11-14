05f4ed88b3c07ede397c9172537a4ad39d43a9ca
jonghyeok-97 <dkswhdgur1209@naver.com>
2023-11-14 11:28:17 +0900

    refactor: 1일당 100원씩 증가하는 기능의 확인하는 테스트코드의 매개변수 변경

--- src/test/java/christmas/NormalDiscountTest.java
+++ src/test/java/christmas/NormalDiscountTest.java
@@ -13,10 +13,9 @@ public class NormalDiscountTest {
 
     @ParameterizedTest
     @DisplayName("1일-1000원,2일-1100원,3일-1200원 처럼 1일 증가마다 100원씩 증가하는지 테스트")
-    @CsvSource(value = {"바비큐립-2:3:1200", "아이스크림-1:3:0", "바비큐립-1:9:1800",
-            "바비큐립-1:10:1900", "바비큐립-1:26:0"}, delimiter = ':')
-    void check_NORMAL_discount_by_Date(String menu, String date, int discountBenefit) {
-        NormalDiscount normalDiscount = new NormalDiscount(new VisitDate(date), new Order(menu));
+    @CsvSource(value = {"3:1200", "9:1800", "10:1900", "26:0"}, delimiter = ':')
+    void check_NORMAL_discount_by_Date(String date, int discountBenefit) {
+        NormalDiscount normalDiscount = new NormalDiscount(new VisitDate(date));
 
         int actual = normalDiscount.getPrice();
         int expected = discountBenefit;
