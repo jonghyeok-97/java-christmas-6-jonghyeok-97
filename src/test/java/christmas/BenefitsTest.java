1d613ae7820a97cffe6fadb38cc2294d386d6c9f
jonghyeok-97 <dkswhdgur1209@naver.com>
2023-11-14 11:22:51 +0900

    test: 총주문금액이 최소금액을 넘으면 할인이 적용되는 테스트 추가

--- src/test/java/christmas/BenefitsTest.java
+++ src/test/java/christmas/BenefitsTest.java
@@ -27,7 +27,23 @@ public class BenefitsTest {
     @ParameterizedTest
     @DisplayName("총주문금액이 최소금액을 넘지 않으면 할인혜택들이 적용 안되어야 하는 테스트")
     @ValueSource(strings = {"아이스크림-1", "양송이수프-1,제로콜라-1", "타파스-1"})
-    void sum_discount_price_by_date(String menu) {
+    void underMinOrderedAmount(String menu) {
+        VisitDate visitDate = new VisitDate("3");
+        Order order = new Order(menu);
+        Benefits benefit = new Benefits(order, new GiftEvent(order), new NormalDiscount(visitDate, order),
+                new WeekendDiscount(visitDate, order), new WeekdaysDiscount(visitDate, order),
+                new SpecialDiscount(visitDate, order));
+
+        Map<String, Integer> actual = benefit.findHistory();
+        Map<String, Integer> expected = new HashMap<>();
+
+        Assertions.assertThat(actual).isEqualTo(expected);
+    }
+
+    @ParameterizedTest
+    @DisplayName("총주문금액이 최소금액을 넘으면 할인혜택들이 적용 되는 테스트")
+    @ValueSource(strings = {"아이스크림-1,바비큐립-2"})
+    void overMinOrderedAmount(String menu) {
         VisitDate visitDate = new VisitDate("3");
         Order order = new Order(menu);
         Benefits benefit = new Benefits(order, new GiftEvent(order), new NormalDiscount(visitDate, order), new WeekendDiscount(visitDate, order),
@@ -35,6 +51,9 @@ public class BenefitsTest {
 
         Map<String, Integer> actual = benefit.findHistory();
         Map<String, Integer> expected = new HashMap<>();
+        expected.put("크리스마스 디데이 할인:", 1200);
+        expected.put("평일 할인:", 2023);
+        expected.put("특별 할인:", 1000);
 
         Assertions.assertThat(actual).isEqualTo(expected);
     }
