bc7b423c4b48dcd233f815275cd91ae973c5c604
jonghyeok-97 <dkswhdgur1209@naver.com>
2023-11-14 11:29:49 +0900

    refactor: 1일당 100원씩 증가하는 기능 테스트에 경계값 추가

--- src/test/java/christmas/NormalDiscountTest.java
+++ src/test/java/christmas/NormalDiscountTest.java
@@ -1,9 +1,7 @@
 package christmas;
 
-import christmas.model.Order;
 import christmas.model.VisitDate;
 import christmas.model.dateDiscount.NormalDiscount;
-import christmas.model.dateDiscount.SpecialDiscount;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -12,8 +10,8 @@ import org.junit.jupiter.params.provider.CsvSource;
 public class NormalDiscountTest {
 
     @ParameterizedTest
-    @DisplayName("1일-1000원,2일-1100원,3일-1200원 처럼 1일 증가마다 100원씩 증가하는지 테스트")
-    @CsvSource(value = {"3:1200", "9:1800", "10:1900", "26:0"}, delimiter = ':')
+    @DisplayName("1일-1000원,2일-1100원,3일-1200원 와 같이 25일-3400원 까지 100원씩 증가하는지 테스트")
+    @CsvSource(value = {"3:1200", "9:1800", "10:1900", "25:3400", "26:0"}, delimiter = ':')
     void check_NORMAL_discount_by_Date(String date, int discountBenefit) {
         NormalDiscount normalDiscount = new NormalDiscount(new VisitDate(date));
 
