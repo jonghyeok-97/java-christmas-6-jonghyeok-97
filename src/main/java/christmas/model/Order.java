ffc0d9837a8174d65cc0b7f3e141f615381c6a26
jonghyeok-97 <dkswhdgur1209@naver.com>
2023-11-14 16:01:35 +0900

    refactor: 하드코딩된 값들을 상수화하고, 15줄 이하로 함수 분리

--- src/main/java/christmas/model/Order.java
+++ src/main/java/christmas/model/Order.java
@@ -1,5 +1,6 @@
 package christmas.model;
 
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
@@ -13,65 +14,41 @@ public class Order {
     private static final String ERROR_ONLY_BEVERAGE = "[ERROR] 음료만 주문할 수 없습니다. 다시 입력해 주세요.";
     private static final String ORDER_DELIMETER = ",";
     private static final String MENU_DELIMETER = "-";
+    private static final int NOT_EXIST_LOCATION = -1;
+    private static final int NOT_COUNT = 0;
+    private static final int MAX_COUNT = 20;
+    private static final int MIN_TOTAL_AMOUNT_FOR_DISCOUNT = 10000;
+    private static final int MIN_TOTAL_AMOUNT_FOR_GIFT = 120000;
 
     private final Map<String, Integer> countByOrderedMenu = new HashMap<>();
-    private final int totalPrice;
+    private final int totalAmount;
 
     public Order(String input) throws IllegalArgumentException {
         validateOrder(input);
-        List<String> dashes = Stream.of(input.split(ORDER_DELIMETER))
-                .toList();
-        Set<String> duplicateMenu = new HashSet<>();
-        int totalCount = 0;
-        for (String s : dashes) {
-            int idx = s.indexOf(MENU_DELIMETER);
-            if (idx == -1) {
-                throw new IllegalArgumentException(ERROR_RETRY_ORDER);
-            }
-            String menu = s.substring(0, idx);
-            if (menu.isEmpty()) {
-                throw new IllegalArgumentException(ERROR_RETRY_ORDER);
-            }
-            int count = 0;
-            try {
-                count = Integer.parseInt(s.substring(idx + 1));
-                if (count <= 0) {
-                    throw new NumberFormatException();
-                }
-            } catch (NumberFormatException error) {
-                throw new IllegalArgumentException(ERROR_RETRY_ORDER);
-            }
-            totalCount += count;
-            validateOnMenuBorad(menu);
-
-            duplicateMenu.add(menu);
-
-            countByOrderedMenu.put(menu ,count);
+        List<String> menusWithMenuDelimeter = splitWithOrderDelimeter(input);
+        for (String menuWithDelimeter : menusWithMenuDelimeter) {
+            int locationOfDelimeter = findLocationOfMenuDelimeter(menuWithDelimeter);
+            String menu = extractMenu(menuWithDelimeter, locationOfDelimeter);
+            int count = extractCount(menuWithDelimeter, locationOfDelimeter);
+            this.countByOrderedMenu.put(menu ,count);
         }
-        if (dashes.size() != duplicateMenu.size()) {
-            throw new IllegalArgumentException(ERROR_RETRY_ORDER);
-        }
-        if (totalCount > 20) {
-            throw new IllegalArgumentException(ERROR_MAX_MENU_COUNT);
-        }
-        validateOnlyBeverage();
-        this.totalPrice = calculateTotalAmount();
-
+        validateMenu(menusWithMenuDelimeter);
+        this.totalAmount = calculateTotalAmount();
     }
 
-    public int getTotalPrice() {
-        return totalPrice;
+    public int getTotalAmount() {
+        return totalAmount;
     }
 
     public Map<String, Integer> getCountByOrderedMenu() {
-        return countByOrderedMenu;
+        return Collections.unmodifiableMap(countByOrderedMenu);
     }
 
     public boolean isOverMinAmount() {
-        return totalPrice >= 10000;
+        return totalAmount >= MIN_TOTAL_AMOUNT_FOR_DISCOUNT;
     }
-    public boolean isOverMinPresentPrice() {
-        return totalPrice >= 120000;
+    public boolean isOverMinGiftAmount() {
+        return totalAmount >= MIN_TOTAL_AMOUNT_FOR_GIFT;
     }
 
     public int countDessertMenu() {
@@ -97,7 +74,7 @@ public class Order {
         Stream.of(input.split(ORDER_DELIMETER))
                 .filter(String::isEmpty)
                 .findAny()
-                .ifPresent(splited -> {
+                .ifPresent(invalidOrder -> {
                     throw new IllegalArgumentException(ERROR_RETRY_ORDER);
                 });
     }
@@ -108,10 +85,39 @@ public class Order {
         }
     }
 
-    private void validateOnMenuBorad(String menu) {
-        if (!MenuBoard.contains(menu)) {
+    private void validateMenu(List<String> menusWithMenuDelimeter) {
+        validateDuplicatedMenu(menusWithMenuDelimeter);
+        validateMenuExistOnMenuBorad();
+        validateTotalCountOfAllMenu();
+        validateOnlyBeverage();
+    }
+
+    private void validateDuplicatedMenu(List<String> menusWithMenuDelimeter) {
+        Set<String> duplicacy = new HashSet<>(this.countByOrderedMenu.keySet());
+        if (menusWithMenuDelimeter.size() != duplicacy.size()) {
             throw new IllegalArgumentException(ERROR_RETRY_ORDER);
         }
+    }
+
+    private void validateMenuExistOnMenuBorad() {
+        this.countByOrderedMenu.keySet().stream()
+                .filter(menu -> !MenuBoard.contains(menu))
+                .findAny()
+                .ifPresent(notExistOnMenuBoard -> {
+                    throw new IllegalArgumentException(ERROR_RETRY_ORDER);
+                });
+    }
+
+    private void validateTotalCountOfAllMenu() {
+        if (sumCountOfAllMenu() > MAX_COUNT) {
+            throw new IllegalArgumentException(ERROR_MAX_MENU_COUNT);
+        }
+    }
+
+    private int sumCountOfAllMenu() {
+        return this.countByOrderedMenu.keySet().stream()
+                .mapToInt(this::countOf)
+                .sum();
     }
 
     private void validateOnlyBeverage() {
@@ -123,6 +129,52 @@ public class Order {
     private boolean hasDishFromMenu() {
         return this.countByOrderedMenu.keySet().stream()
                 .anyMatch(MenuBoard::findDish);
+    }
+
+    private List<String> splitWithOrderDelimeter(String input) {
+        return Stream.of(input.split(ORDER_DELIMETER))
+                .toList();
+    }
+
+    private int findLocationOfMenuDelimeter(String orderedMenu) {
+        int location = orderedMenu.indexOf(MENU_DELIMETER);
+        validateFound(location);
+        return location;
+    }
+
+    private void validateFound(int location) {
+        if (location == NOT_EXIST_LOCATION) {
+            throw new IllegalArgumentException(ERROR_RETRY_ORDER);
+        }
+    }
+
+    private String extractMenu(String menuWithDelimeter, int locationOfDelimeter) {
+        String menu = menuWithDelimeter.substring(0, locationOfDelimeter);
+        validateEmpty(menu);
+        return menu;
+    }
+
+    private void validateEmpty(String menu) {
+        if (menu.isEmpty()) {
+            throw new IllegalArgumentException(ERROR_RETRY_ORDER);
+        }
+    }
+
+    private int extractCount(String menuWithDelimeter, int locationOfDelimeter) {
+        String extractedCount = menuWithDelimeter.substring(locationOfDelimeter + 1);
+        try {
+            int count = Integer.parseInt(extractedCount);
+            validateExist(count);
+            return count;
+        } catch (NumberFormatException error) {
+            throw new IllegalArgumentException(ERROR_RETRY_ORDER);
+        }
+    }
+
+    private void validateExist(int count) {
+        if (count <= NOT_COUNT) {
+            throw new IllegalArgumentException(ERROR_RETRY_ORDER);
+        }
     }
 
     private int calculateTotalAmount() {
