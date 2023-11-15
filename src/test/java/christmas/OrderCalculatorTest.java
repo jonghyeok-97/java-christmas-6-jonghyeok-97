efe3781e188e1b5debf4dd4f2241feab33b643f6
jonghyeok-97 <dkswhdgur1209@naver.com>
2023-11-15 18:58:58 +0900

    test: 주문구분자(-)로 나눌때의 구분자의 위치 테스트 추가

--- src/test/java/christmas/OrderCalculatorTest.java
+++ src/test/java/christmas/OrderCalculatorTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class OrderCalculatorTest {
     private OrderCalculator orderCalculator;
@@ -18,8 +19,7 @@ public class OrderCalculatorTest {
 
     @ParameterizedTest
     @DisplayName("주문구분자(,)로 입력값이 나눠지는지 테스트")
-    @CsvSource(value = {"바비큐립-3,제로콜라-4", "타파스-3,양송이수프-1,샴페인-3", "초코케이크-10,제로콜라-10"},
-            delimiter = ':')
+    @ValueSource(strings = {"바비큐립-3,제로콜라-4", "타파스-3,양송이수프-1,샴페인-3", "초코케이크-10,제로콜라-10"})
     void split_With_Menu_Delimeter(String menu) {
         List<String> actual = orderCalculator.splitWithOrderDelimeter(menu);
         List<String> expected = List.of(menu.split(","));
@@ -27,5 +27,14 @@ public class OrderCalculatorTest {
         Assertions.assertThat(actual).isEqualTo(expected);
     }
 
+    @ParameterizedTest
+    @DisplayName("메뉴구분자(-)의 위치를 확인하는 테스트")
+    @CsvSource(value = {"바비큐립-3,4", "크리스마스파스타-3,8", "초코케이크-10,5"})
+    void find_Location_Of_Menu_Delimeter(String menu, int location) {
+        int actual = orderCalculator.findLocationOfMenuDelimeter(menu);
+        int expected = location;
+
+        Assertions.assertThat(actual).isEqualTo(expected);
+    }
 
 }
