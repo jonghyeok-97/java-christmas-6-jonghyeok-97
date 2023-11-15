4f0285523d9812a121e731765a44adcd974c586a
jonghyeok-97 <dkswhdgur1209@naver.com>
2023-11-15 19:17:28 +0900

    refactor: 테스트 가독성을 위해 실제값과 예상값 추가

--- src/test/java/christmas/GiftEventTest.java
+++ src/test/java/christmas/GiftEventTest.java
@@ -1,21 +1,33 @@
 package christmas;
 
 import christmas.model.GiftEvent;
+import christmas.model.Order;
 import christmas.model.OrderGenerator;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 public class GiftEventTest {
+    private OrderGenerator orderGenerator;
+
+    @BeforeEach
+    void set() {
+        orderGenerator = new OrderGenerator();
+
+    }
 
     @ParameterizedTest
     @ValueSource(strings = {"해산물파스타-15,레드와인-1", "바비큐립-3", "양송이수프-1,바비큐립-1,레드와인-1"})
     @DisplayName("총주문금액이 12만원이 넘어가면 증정하는 테스트")
     void is_over_present_price(String menus) {
-        OrderGenerator orderGenerator = new OrderGenerator();
-        GiftEvent gift = new GiftEvent(orderGenerator.createCountByOrdereMenu(menus));
+        Order order = orderGenerator.createCountByOrdereMenu(menus);
+        GiftEvent gift = new GiftEvent(order);
 
-        Assertions.assertThat(gift.getFree()).isEqualTo(true);
+        boolean actual = gift.getFree();
+        boolean expected = true;
+
+        Assertions.assertThat(actual).isEqualTo(expected);
     }
 }
